187128230b1dc0d86ee9b23f81fb30c2e4e5ebda
josh mitchell <jshmtchll@gmail.com>
2021-03-01 17:06:32 -0600

    added questions array to js and hit other sections from css file until called in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,5 +14,66 @@ var asw2Btn = document.querySelector("#asw2");
 var asw3Btn = document.querySelector("#asw3");
 var asw4Btn = document.querySelector("#asw4");
 var submitBtn = document.querySelector("#score-submit");
-var playAgainBtn = document.querySelector("#play-again")
+var restartBtn = document.querySelector("#play-again");
+
+//q&a section
+var questionsEl = document.querySelector("#questions");
+// <h2> that holds the question for user
+var questionEl = document.querySelector("#question");
+// for div providing feedback if answer is right/wrong
+var correctWrong = document.querySelector("#correct-wrong");
+
+//post quiz section
+var scoreEl = document.querySelector("#final-score");
+var playerScore = document.querySelector("#player-score");
+var playerInitials = document.querySelector("#initials-input");
+
+//displays scores after user input inititals
+var viewHighscoresEl = document.querySelector("#hiscores");
+var highscoreListEl = document.querySelector("#hiscore-list");
+var highscoreList = [];
+
+var questions = [
+    
+    {
+        question: "What is the correct JavaScript syntax to change the content of the HTML element below?",
+        answers: ["1. #demo.innerHTML = 'Hello World!';", 
+                "2. document.getElement('p').innerHTML = 'Hello World!';", 
+                "3. document.getElementById('demo').innerHTML = 'Hello World!';", 
+                "4. document.getElementByName('p').innerHTML = 'Hello World!';"],
+        correctAnswer: "3" 
+    },  
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: ["1. The <body> section", 
+                  "2. Both the <head> section and the <body> sections",
+                  "3. The <head> section",
+                  "4. The <footer> section"],
+        correctAnswer: "2"        
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: ["1. msg('Hello World');",
+                  "2. alertBox('Hello World');",
+                  "3. alert('Hello World');",
+                  "4. msgBox('Hello World');"],
+        correctAnswer: "3"
+    },  
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: ["1. var colors = 'red', 'green', 'blue'",
+                  "2. var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+                  "3. var colors = (1:'red', 2:'green', 3:'blue')",
+                  "4. var colors = ['red', 'green', 'blue']  "],
+        correctAnswer: "4"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        answers: ["1. ceil(x, y)",
+                  "2. Math.max(x, y)",
+                  "3. top(x, y)",
+                  "4. Math.ceil(x, y)"],
+        correctAnswer: "2"
+    }
+];
 
