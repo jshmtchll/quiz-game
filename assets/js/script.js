e6fb35d81f049bc2dee58346083dbe172dfb4f17
josh mitchell <jshmtchll@gmail.com>
2021-03-01 15:44:59 -0600

    finished html for now

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//page elemnts
+//timer
+var timerEl = document.querySelector("#timer");
+var timeLeft = 80;
+
+//intro screen
+var introEl = document.querySelector("#intro");
+
+//buttons
+var viewHighscores = document.querySelector("#view-hiscore");
+var startBtn = document.querySelector("#start");
+var asw1Btn = document.querySelector("#asw1");
+var asw2Btn = document.querySelector("#asw2");
+var asw3Btn = document.querySelector("#asw3");
+var asw4Btn = document.querySelector("#asw4");
+var submitBtn = document.querySelector("#score-submit");
+var playAgainBtn = document.querySelector("#play-again")
+
